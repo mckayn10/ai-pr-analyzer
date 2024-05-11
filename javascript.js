aed5a85add005ff12ca8c949e8bbc6298b4f79b2
McKay Nilsson <mckaynilsson@mckays-mbp.lan>
2024-05-11 12:02:37 -0600

    add new code

--- javascript.js
+++ javascript.js
@@ -92,3 +92,19 @@ function updateCartDisplay(cart) {
 	});
 	console.log('Cart display updated.');
 }
+
+function displayCart() {
+	const cart = JSON.parse(localStorage.getItem('cart')) || [];
+	updateCartDisplay(cart);
+}
+
+function updateCartDisplay(cart) {
+	const cartDisplay = document.getElementById('cart');
+	cartDisplay.innerHTML = ''; // Clear current display
+	cart.forEach((item) => {
+		const productElement = document.createElement('div');
+		productElement.textContent = `${item.name}: ${item.quantity}`;
+		cartDisplay.appendChild(productElement);
+	});
+	console.log('Cart display updated.');
+}
