1eb57b228474327754e357718d9859e26b5004ba
McKay Nilsson <mckaynilsson@mckays-mbp.lan>
2024-05-11 13:23:31 -0600

    updated

--- javascript.js
+++ javascript.js
@@ -70,20 +70,3 @@ function displayCart() {
 	const cart = JSON.parse(localStorage.getItem('cart')) || [];
 	updateCartDisplay(cart);
 }
-
-function updateCartDisplay(cart) {
-	const cartDisplay = document.getElementById('cart');
-	cartDisplay.innerHTML = ''; // Clear current display
-	cart.forEach((item) => {
-		const productElement = document.createElement('div');
-		productElement.textContent = `${item.name}: ${item.quantity}`;
-		cartDisplay.appendChild(productElement);
-	});
-	console.log('Cart display updated.');
-}
-
-function thisDoesNothing() {}
-
-function thisDoesNothing() {}
-function thisDoesNothing() {}
-function thisDoesNothing() {}
