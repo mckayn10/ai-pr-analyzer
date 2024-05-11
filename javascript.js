639149be9a9a153666227189d2bf3f70e9c249f3
McKay Nilsson <mckaynilsson@mckays-mbp.lan>
2024-05-11 11:01:52 -0600

    add process transaction function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,30 @@
 // this is where all the javascript code will be stored
 
+// function processTransaction(amount, account) {
+// 	// Validate transaction
+// 	if (amount <= 0) {
+// 		console.error('Transaction amount must be greater than zero.');
+// 		return false;
+// 	}
+// 	if (!account.isActive) {
+// 		console.error('Account is inactive.');
+// 		return false;
+// 	}
+
+// 	// Calculate new balance
+// 	const newBalance = account.balance + amount;
+
+// 	// Log transaction
+// 	console.log(
+// 		`Transaction for ${amount} processed. New balance: ${newBalance}.`
+// 	);
+
+// 	// Update account balance
+// 	account.balance = newBalance;
+
+// 	return true;
+// }
+
 function processTransaction(amount, account) {
 	// Validate transaction
 	if (amount <= 0) {
